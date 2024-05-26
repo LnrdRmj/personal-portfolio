66585edd299895a691905a34d1efb74dbfd2a7ba
LnrdRmj <51125018+LnrdRmj@users.noreply.github.com>
2024-05-26 21:32:22 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
 </script>
 
 <template>
-    <div class="bg-green-500 text-5xl h-full w-full flex-center">
-        Ciao Caterina
+    <div class="flex flex-col w-full h-full overflow-scroll">
+        <div class="flex flex-col h-screen w-full shrink-0">
+            <Header></Header>
+            <div class="bg-green-500 text-8xl h-full w-full flex-center font-bold">
+                Ciao
+            </div>
+        </div>
+        <div class="bg-red-500 h-32 w-full shrink-0">
+            ciao
+        </div>
     </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="bg-black flex text-white text-2xl py-2 px-10">
+        <div>
+            Leonardo Ramaj
+        </div>
+    </div>
+</template>
