fedb492dec86b78c0deae8740ee8e54c90f3d397
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-26 22:55:06 +0200

    graphic changes

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,26 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-    <div class="flex flex-col w-full h-full overflow-scroll">
+    <div class="flex flex-col w-full h-full overflow-y-scroll bg-primary">
         <div class="flex flex-col h-screen w-full shrink-0">
-            <Header></Header>
-            <div class="bg-green-500 text-8xl h-full w-full flex-center font-bold">
-                Ciao
+            <Header class="fixed w-full"></Header>
+            <div class="flex flex-col pl-36">
+                <div class="text-7xl w-full font-semibold max-w-[1100px] mt-36">
+                    <div>Ciao, mi chiamo <br> Iacopo Pazzaglia e sono un Creative Director e <br> Web Designer</div>
+                </div>
+                <div class="text-2xl mt-12">
+                    <div>
+                        Pronto a trasformare la tua visione digitale in realtà?
+                    </div>
+                    <div>
+                        Contattami oggi per prenotare una consulenza gratuita e discutere del tuo progetto.
+                    </div>
+                </div>
+
+                <button class="px-8 w-fit rounded-lg mt-24 bg-secondary py-3 text-white text-xl font-semibold">
+                    Conosciamoci!
+                </button>
+
             </div>
         </div>
         <div class="bg-red-500 h-32 w-full shrink-0">
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+
+import WhiteArrow from '../assets/white_arrow.svg'
+
 </script>
 
 <template>
-    <div class="bg-black flex text-white text-2xl py-2 px-10">
+    <div class="bg-black flex justify-between text-white text-xl py-5 px-10">
         <div>
-            Leonardo Ramaj
+            Iacopo Pazzaglia
+        </div>
+        <div class="flex justify-between items-center space-x-20">
+            <button> Work </button>
+            <button> Servizi </button>
+            <button> Chi sono </button>
+            <button class="rounded-full border flex items-center border-primary py-1 px-4">
+                <div class="mr-2">
+                    Contattami
+                </div>
+                <div>
+                    <img :src="WhiteArrow" alt="" srcset="">
+                </div>
+            </button>
         </div>
     </div>
 </template>
