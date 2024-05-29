c9fb75a172a107a2c87bcd5fcfbb380d846d5e9b
LnrdRmj <leonardo.ra14@icloud.com>
2024-05-29 11:29:28 +0200

    works component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,58 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
-import FeaturedWorks from './components/landingPage/feturedWorks/FeaturedWorks.vue'
-import Services from './components/landingPage/services/Services.vue'
+import Header from './components/Header.vue';
+import FeaturedWorks from './components/landingPage/feturedWorks/FeaturedWorks.vue';
+import Services from './components/landingPage/services/Services.vue';
+import Works from './components/landingPage/works/Works.vue'
+
+const works = [
+    {
+        period: '2021 - present',
+        clientName: 'Yoomy',
+        role: 'Co-Owner / Creative Director',
+        skills: [
+            { name: 'APP', color: '#A7FF7D' },
+            { name: 'BRAND', color: '#79F7FF' },
+            { name: '3D', color: '#FFA665' }
+        ]
+    },
+    {
+        period: '2022 - present',
+        clientName: 'Voricel',
+        role: 'Web Designer',
+        skills: [
+            { name: 'WEB', color: '#EAFF6A' },
+            { name: 'BRAND', color: '#79F7FF' },
+            { name: 'ADS', color: '#FF72E0' }
+        ]
+    },
+    {
+        period: '2022 - present',
+        clientName: 'Naturae Firenze',
+        role: 'Graphic Designer',
+        skills: [
+            { name: 'VISUAL', color: '#FF6A6A' },
+            { name: 'SOCIAL', color: '#CE90FF' }
+        ]
+    },
+    {
+        period: '2023 - present',
+        clientName: 'Softkeys',
+        role: 'Graphic Designer',
+        skills: [
+            { name: 'ADS', color: '#FF72E0' }
+        ]
+    },
+    {
+        period: '2023 - 2024',
+        clientName: 'i\'Re',
+        role: 'Visual Designer',
+        skills: [
+            { name: 'VISUAL', color: '#FF6A6A' },
+            { name: 'BRAND', color: '#79F7FF' }
+        ]
+    }
+];
+
 </script>
 
 <template>
@@ -31,8 +82,10 @@ import Services from './components/landingPage/services/Services.vue'
             <div class="w-full bg-slate-400 h-[2px] shrink-0 mb-5"/>
             <FeaturedWorks/>
         </div>
-        <div class="h-96 shrink-0 px-3 mt-64">
+        <div class="shrink-0 px-3 mt-64 mb-72">
             <Services/>
         </div>
+        <div class="max-w-full bg-gray-400 h-[2px] shrink-0 mb-5 mx-20"/>
+        <Works :works="works" class="pb-24 max-w-full mx-20"/>
     </div>
 </template>
