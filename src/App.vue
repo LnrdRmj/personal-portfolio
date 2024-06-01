b7901d98c61138f3747697876dd8fbd3e3f8f46e
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-01 13:51:40 +0200

    translations for lading title and description

--- src/App.vue
+++ src/App.vue
@@ -66,14 +66,16 @@ const works = [
             <Header class="fixed w-full z-20"></Header>
             <div class="flex flex-col pl-36">
                 <div class="text-7xl w-full font-semibold max-w-[1100px] mt-36">
-                    <div>Ciao, mi chiamo <br> Iacopo Pazzaglia e sono un Creative Director e <br> Web Designer</div>
+                    <div>{{ $t('title') }}</div>
                 </div>
                 <div class="text-2xl mt-12">
                     <div>
-                        Pronto a trasformare la tua visione digitale in realtà?
+                        {{ $t('underTitle.phrase1')}}
+                        <!-- Pronto a trasformare la tua visione digitale in realtà? -->
                     </div>
                     <div>
-                        Contattami oggi per prenotare una consulenza gratuita e discutere del tuo progetto.
+                        {{ $t('underTitle.phrase2')}}
+                        <!-- Contattami oggi per prenotare una consulenza gratuita e discutere del tuo progetto. -->
                     </div>
                 </div>
 
