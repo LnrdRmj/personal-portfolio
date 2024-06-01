b0c64b05b2525ccd065e21c99194b383dcb90864
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-01 17:48:55 +0200

    autodetect languages

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,10 +1,11 @@
 import i18next from 'i18next';
 import en from '../i18n/en.json';
 import it from '../i18n/it.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 export function setup() {
-    i18next.init({
-        lng: 'it',
+    i18next.use(LanguageDetector).init({
+        supportedLngs: ['en', 'it'],
         interpolation: {
             escapeValue: false
         },
