6800a757447b24bd467b19a6393afab151fc2c02
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-13 18:41:08 +0200

    cool clip text effect

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,12 @@ i18next.on("languageChanged", () => {
     <div class="flex flex-col w-full h-full overflow-y-scroll bg-primary">
         <div class="flex flex-col h-screen w-full shrink-0">
             <Header class="fixed w-full z-20"></Header>
-            <div class="flex flex-col pl-36">
-                <div class="text-7xl w-full font-semibold max-w-[1100px] mt-36">
-                    <div>{{ $t('title') }}</div>
+            <div class="flex flex-col px-16 sm:px-36">
+                <div class="text-3xl sm:text-5xl lg:text-6xl xl:text-7xl w-full
+                            font-medium sm:font-semibold
+                            max-w-[1100px] mt-36">
+                    <div v-html="$t('title')"></div>
+                    <div class="hidden test-transparent animate-background-scroll  bg-gradient-to-tl from-amber-500 to-pink-500 bg-clip-text"></div>
                 </div>
                 <div class="text-2xl mt-12">
                     <div>
