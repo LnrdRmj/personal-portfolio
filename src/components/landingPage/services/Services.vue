650016687c266afe9c5dd2e6318edb8ec888f9a9
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-13 19:03:54 +0200

    more responsiveness for services

--- src/components/landingPage/services/Services.vue
+++ src/components/landingPage/services/Services.vue
@@ -8,11 +8,11 @@ import ServiceCard from './ServiceCard.vue';
 </script>
 
 <template>
-    <div class="flex flex-col rounded-[20px] px-16 pt-[120px] pb-24 bg-secondary w-full text-white">
+    <div class="flex flex-col rounded-[20px] px-5 sm:px-16 pt-[120px] pb-24 bg-secondary w-full text-white">
         <div class="w-full bg-zinc-800 h-[2px] shrink-0 mb-5"/>
         <div class="uppercase font-normal text-2xl mb-[80px]">{{ $t('services.title') }}</div>
         <div class="flex flex-col space-x-0 space-y-10 sm:flex-row sm:space-x-10 sm:space-y-0 w-full [&>*]:h-[700px]">
-            <ServiceCard class="flex-1" 
+            <ServiceCard class="flex-1"
                 :description="$t('services.service1.description')"
                 :skills="$t('services.service1.skills', { returnObjects: true })"
                 :imageSrc="Image1"/>
