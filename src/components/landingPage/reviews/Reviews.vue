4493413cfec6ea0b2d711b7fdf2cbcb467105675
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-14 13:32:07 +0200

    responsive for reviews component

--- src/components/landingPage/reviews/Reviews.vue
+++ src/components/landingPage/reviews/Reviews.vue
@@ -75,41 +75,49 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="flex flex-col bg-secondary rounded-[20px] px-16">
+    <div class="flex flex-col bg-secondary rounded-[20px] px-5 sm:px-16">
         <div class="w-full bg-zinc-800 h-[2px] shrink-0 mt-[50px] mb-4"/>
         <div class="uppercase font-normal text-2xl mb-[80px] text-white">
             COSA DICONO I MIEI CLIENTI
         </div>
-        <div class="flex h-[650px] space-x-5">
-            <div class="flex flex-col space-y-2">
+        <div class="flex flex-col
+            sm:flex-row sm:h-[650px] sm:space-x-5">
+            <div class="
+                flex flex-wrap
+                sm:flex-col sm:flex-nowrap sm:space-y-2">
                 <Client v-for="(review, index) of reviews"
+                    class="mr-3 mb-3"
                     @click="selectedReviewIndex = index"
                     :clientName="review.companyName"
                     :imageSrc="review.logo"
                     :selected="index === selectedReviewIndex"/>
-                <!-- <Client clientName="i'Re" :imageSrc="IreLogo" :selected="false"/> -->
             </div>
-            <div class="w-[38%]">
-                <div class="bg-green-400 size-full rounded-[20px]"></div>
-            </div>
-
-            <div class="relative w-[38%] flex flex-1">
-                <div v-for="(review, index) in reviews" :key="index"
-                    class="w-full flex h-full absolute left-0 top-0 px-10"
-                    :class="index === selectedReviewIndex ? 'z-10' : ''">
-                    <Transition name="review-switch" mode="in-out" :duration="1000">
-                        <Review class="w-full"
-                            v-if="index === selectedReviewIndex"
-                            v-bind="{
-                                review: review.review,
-                                name: review.reviewer,
-                                role: review.role
-                            }"/>
-                    </Transition>
+            <div class="flex flex-col w-full lg:flex-row">
+                <div class="w-full h-56
+                            lg:h-auto lg:w-[50%]">
+                    <div class="bg-green-400 size-full rounded-[20px]"></div>
+                </div>
+    
+                <div class="relative max-h-96 h-96 w-full mt-5
+                    sm:h-auto flex sm:flex-1 sm:max-h-none
+                    lg:w-[50%]">
+                    <div v-for="(review, index) in reviews" :key="index"
+                        class="w-full flex h-full absolute left-0 top-0 px-0 lg:px-10"
+                        :class="index === selectedReviewIndex ? 'z-10' : ''">
+                        <Transition name="review-switch" mode="in-out" :duration="1000">
+                            <Review class="w-full"
+                                v-if="index === selectedReviewIndex"
+                                v-bind="{
+                                    review: review.review,
+                                    name: review.reviewer,
+                                    role: review.role
+                                }"/>
+                        </Transition>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="w-full bg-zinc-800 h-[2px] shrink-0 my-16"/>
+        <div class="bg-transparent w-full sm:bg-zinc-800 h-[2px] shrink-0 my-16"/>
     </div>
 </template>
 
