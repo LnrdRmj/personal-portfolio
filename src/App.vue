1584199665187f3bda4e3d97fa0aa8de6823fad5
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-29 16:54:01 +0200

    reorganized to prevent nasty bug

--- src/App.vue
+++ src/App.vue
@@ -24,79 +24,81 @@ i18next.on("languageChanged", () => {
 </script>
 
 <template>
-    <div class="flex flex-col w-full h-full overflow-y-scroll bg-primary">
-        <div class="flex flex-col h-screen w-full shrink-0">
-            <Header class="fixed w-full z-20"></Header>
-            <div class="flex flex-col px-10 sm:px-36">
-                <div class="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl w-full
-                            font-medium sm:font-semibold
-                            max-w-[1100px] mt-36">
-                    <div v-html="$t('title')"></div>
-                </div>
-                <div class="text-xl mt-12">
-                    <div>
-                        {{ $t('underTitle.phrase1') }}
+    <div>
+        <Header class="fixed w-full z-20"></Header>
+        <div class="flex flex-col w-full h-full overflow-y-scroll bg-primary">
+            <div class="flex flex-col h-screen w-full shrink-0">
+                <div class="flex flex-col px-10 sm:px-36">
+                    <div class="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl w-full
+                                font-medium sm:font-semibold
+                                max-w-[1100px] mt-36">
+                        <div v-html="$t('title')"></div>
                     </div>
-                    <div>
+                    <div class="text-xl mt-12">
+                        <div>
+                            {{ $t('underTitle.phrase1') }}
+                        </div>
+                        <div>
+                            {{ $t('underTitle.phrase2') }}
+                        </div>
+                    </div>
+
+                    <button class="px-8 w-fit rounded-lg mt-24 bg-secondary py-3 text-white text-xl font-semibold
+                        hover:bg-blue-600 transition-colors">
+                        {{ $t('knoweachotherbutton') }}
+                    </button>
+
+                </div>
+            </div>
+            <div class="mb-32">
+                <Slider />
+            </div>
+            <div class="w-full shrink-0 px-10 sm:px-36">
+                <div class="w-full bg-gray-400 h-[2px] shrink-0 mb-5" />
+                <FeaturedWorks />
+            </div>
+            <div class="shrink-0 px-3 mt-64 mb-72">
+                <Services />
+            </div>
+            <div class="max-w-full bg-gray-400 h-[2px] shrink-0 mb-5 mx-10 sm:mx-36" />
+            <Works :works="works" class="px-10 sm:px-36 max-w-full mb-32" />
+            <div class="px-3 shrink-0">
+                <Reviews />
+            </div>
+            <div class="flex flex-col px-10 sm:px-20 lg:px-36 my-52">
+                <div class="max-w-full bg-gray-400 h-[2px] shrink-0 mb-5" />
+                <div class="uppercase text-2xl mb-16">{{ $t('contactsSection.title') }}</div>
+                <div class="flex flex-col">
+                    <div class="text-4xl sm:text-5xl lg:text-6xl font-semibold">
+                        {{ $t('contactMe') }}
+                    </div>
+                    <div class="text-lg leading-5 mt-5">
+                        {{ $t('underTitle.phrase1') }}<br>
                         {{ $t('underTitle.phrase2') }}
                     </div>
                 </div>
-
-                <button class="px-8 w-fit rounded-lg mt-24 bg-secondary py-3 text-white text-xl font-semibold
+                <div class="mt-10">
+                    <div class="flex">
+                        <img class="h-4 mt-[5px] mr-2" :src="WhatsappIcon" alt="">
+                        <div class="flex flex-col font-semibold">
+                            <div class="text-lg">Whatsapp</div>
+                            <div>+39 327 955 1219</div>
+                        </div>
+                    </div>
+                    <div class="flex mt-6">
+                        <img class="h-4 mt-[5px] mr-2" :src="EmailIcon" alt="">
+                        <div class="flex flex-col font-semibold">
+                            <div class="text-lg">Email</div>
+                            <div>leonardo.ra14@icloud.com</div>
+                        </div>
+                    </div>
+                </div>
+                <button class="px-8 w-fit rounded-lg mt-20 bg-secondary py-3 text-white text-base lg:text-xl font-semibold
                     hover:bg-blue-600 transition-colors">
-                    {{ $t('knoweachotherbutton') }}
+                    {{ $t('contactsSection.bookConsultation') }}
                 </button>
-
             </div>
+            <Footer />
         </div>
-        <div class="mb-32">
-            <Slider />
-        </div>
-        <div class="w-full shrink-0 px-10 sm:px-36">
-            <div class="w-full bg-gray-400 h-[2px] shrink-0 mb-5" />
-            <FeaturedWorks />
-        </div>
-        <div class="shrink-0 px-3 mt-64 mb-72">
-            <Services />
-        </div>
-        <div class="max-w-full bg-gray-400 h-[2px] shrink-0 mb-5 mx-10 sm:mx-36" />
-        <Works :works="works" class="px-10 sm:px-36 max-w-full mb-32" />
-        <div class="px-3 shrink-0">
-            <Reviews />
-        </div>
-        <div class="flex flex-col px-10 sm:px-20 lg:px-36 my-52">
-            <div class="max-w-full bg-gray-400 h-[2px] shrink-0 mb-5" />
-            <div class="uppercase text-2xl mb-16">{{ $t('contactsSection.title') }}</div>
-            <div class="flex flex-col">
-                <div class="text-4xl sm:text-5xl lg:text-6xl font-semibold">
-                    {{ $t('contactMe') }}
-                </div>
-                <div class="text-lg leading-5 mt-5">
-                    {{ $t('underTitle.phrase1') }}<br>
-                    {{ $t('underTitle.phrase2') }}
-                </div>
-            </div>
-            <div class="mt-10">
-                <div class="flex">
-                    <img class="h-4 mt-[5px] mr-2" :src="WhatsappIcon" alt="">
-                    <div class="flex flex-col font-semibold">
-                        <div class="text-lg">Whatsapp</div>
-                        <div>+39 327 955 1219</div>
-                    </div>
-                </div>
-                <div class="flex mt-6">
-                    <img class="h-4 mt-[5px] mr-2" :src="EmailIcon" alt="">
-                    <div class="flex flex-col font-semibold">
-                        <div class="text-lg">Email</div>
-                        <div>leonardo.ra14@icloud.com</div>
-                    </div>
-                </div>
-            </div>
-            <button class="px-8 w-fit rounded-lg mt-20 bg-secondary py-3 text-white text-base lg:text-xl font-semibold
-                hover:bg-blue-600 transition-colors">
-                {{ $t('contactsSection.bookConsultation') }}
-            </button>
-        </div>
-        <Footer />
     </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,30 +11,46 @@ watch(language, () => {
     i18next.changeLanguage(language.value).then(console.log).catch(console.log)
 })
 
+const visibility = ref(false)
+
 </script>
 
 <template>
-    <div class="bg-black flex justify-between items-center text-white text-xl py-5 px-10">
-        <div>
-            Leonardo Ramaj
+    <div class="flex flex-col">
+        <div class="bg-black flex justify-between items-center text-white text-xl py-5 px-10 relative z-20">
+            <div>
+                Leonardo Ramaj
+            </div>
+            <div class="hidden lg:flex lg:justify-between lg:items-center lg:space-x-20">
+                <select name="" id="" class="bg-secondary text-white" v-model="language">
+                    <option value="it">IT</option>
+                    <option value="en">ENG</option>
+                </select>
+                <button> {{ $t('headerSection.work') }} </button>
+                <button> {{ $t('headerSection.whoAmI') }} </button>
+                <button> {{ $t('headerSection.services') }} </button>
+                <ContactMe />
+            </div>
+            <div class="lg:hidden text-white">
+                <button @click="visibility = !visibility">Menu</button>
+            </div>
         </div>
-        <div class="flex justify-between items-center space-x-20">
-            <select name="" id="" class="bg-secondary text-white" v-model="language">
-                <option value="it">IT</option>
-                <option value="en">ENG</option>
-            </select>
-            <button> {{ $t('headerSection.work') }} </button>
-            <button> {{ $t('headerSection.whoAmI') }} </button>
-            <button> {{ $t('headerSection.services') }} </button>
-            <!-- <button class="rounded-full border flex items-center border-primary py-1 px-4">
-                <div class="mr-2">
-                    {{ $t('contactMe') }}
-                </div>
-                <div>
-                    <img :src="WhiteArrow" alt="" srcset="">
-                </div>
-            </button> -->
-            <ContactMe/>
+        <!-- <Transition name="to-from-up"> -->
+        <div class="lg:hidden text-white w-full top-full left-0 bg-black z-10 p-12 -mt-[100%] transition-[opacity,transform] duration-[250ms]"
+            :class="visibility ? 'translate-y-full opacity-100' : 'opacity-0 translate-y-0'">
+            <div class="grid grid-cols-1 space-y-4 items-start 
+                size-full 
+                border-t border-t-gray-500 
+                pt-16 px-6
+                text-2xl">
+                <button> {{ $t('headerSection.work') }} </button>
+                <button> {{ $t('headerSection.services') }} </button>
+                <button> {{ $t('headerSection.whoAmI') }} </button>
+                <button> {{ $t('headerSection.contacts') }} </button>
+            </div>
         </div>
+        <!-- </Transition> -->
     </div>
 </template>
+
+<style scoped></style>
