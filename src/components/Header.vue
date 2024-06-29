39f16604a39590683233ca70656188549c232f06
LnrdRmj <leonardo.ra14@icloud.com>
2024-06-29 17:13:52 +0200

    completed header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@ const visibility = ref(false)
 </script>
 
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col relative">
         <div class="bg-black flex justify-between items-center text-white text-xl py-5 px-10 relative z-20">
             <div>
                 Leonardo Ramaj
@@ -35,21 +35,36 @@ const visibility = ref(false)
                 <button @click="visibility = !visibility">Menu</button>
             </div>
         </div>
-        <!-- <Transition name="to-from-up"> -->
-        <div class="lg:hidden text-white w-full top-full left-0 bg-black z-10 p-12 -mt-[100%] transition-[opacity,transform] duration-[250ms]"
-            :class="visibility ? 'translate-y-full opacity-100' : 'opacity-0 translate-y-0'">
-            <div class="grid grid-cols-1 space-y-4 items-start 
-                size-full 
+        <div class="lg:hidden
+            text-white bg-black 
+            absolute top-full left-0
+            w-full h-screen
+            z-10 py-12 px-8
+            transition-[opacity,transform] duration-[250ms]"
+            :class="visibility ? 'translate-y-0 opacity-100' : 'opacity-0 -translate-y-full'">
+            <div class="flex flex-col 
+                w-full 
                 border-t border-t-gray-500 
-                pt-16 px-6
+                pt-20 px-6
                 text-2xl">
-                <button> {{ $t('headerSection.work') }} </button>
-                <button> {{ $t('headerSection.services') }} </button>
-                <button> {{ $t('headerSection.whoAmI') }} </button>
-                <button> {{ $t('headerSection.contacts') }} </button>
+                <div class="flex flex-col items-start space-y-4">
+                    <button> {{ $t('headerSection.work') }} </button>
+                    <button> {{ $t('headerSection.services') }} </button>
+                    <button> {{ $t('headerSection.whoAmI') }} </button>
+                    <button> {{ $t('headerSection.contacts') }} </button>
+                </div>
+            </div>
+            <div class="w-full bg-gray-500 h-[1px] shrink-0 mt-16 mb-10" />
+            <div class="flex flex-col px-6">
+                <div class="text-2xl">
+                    {{ $t('contactMe') }}!
+                </div>
+                <div class="mt-5">
+                    {{ $t('headerSection.menu.catchPhrase') }}
+                </div>
+                <ContactMe class="w-fit mt-16" />
             </div>
         </div>
-        <!-- </Transition> -->
     </div>
 </template>
 
