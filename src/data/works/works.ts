b002a0c04d1d7a27bf3194e2d160ed1327df4383
LnrdRmj <leonardo.ra14@icloud.com>
2024-12-18 17:28:26 +0100

    interfaces

--- src/data/works/works.ts
+++ src/data/works/works.ts
@@ -2,18 +2,28 @@ import { useTranslation } from "i18next-vue";
 import { I18nValue, createI18ValueWithFromOne } from "../../i18n/i18n";
 
 import YoomyBannerProject from "./assets/yoomyBannerProject.png";
+import i18next from "i18next";
 
-const skills = {
+export type Skill = { name: string; color: string };
+const skills: Record<string, Skill> = {
     web: { name: "WEB", color: "#79F7FF" },
     devops: { name: "DEVOPS", color: "#A7FF7D" },
     team: { name: "TEAM", color: "#79F7FF" },
     vue: { name: "VUE", color: "#A7FF7D" },
     app: { name: "APP", color: "#FF72E0" },
     react: { name: "REACT", color: "#CE90FF" },
-};
+} as const;
 
-export function getWorks() {
-    const { t } = useTranslation();
+export interface Work {
+    id: number;
+    period: string;
+    clientName: string;
+    role: I18nValue;
+    skills: Skill[]; // Skills array uses the Skill type
+    bannerPath: string;
+}
+export function getWorks(): Work[] {
+    const { t } = i18next;
     return [
         {
             id: 1,
@@ -22,7 +32,7 @@ export function getWorks() {
             role: {
                 en: "Programmer",
                 it: "Programmatore",
-            } satisfies I18nValue,
+            },
             skills: [skills.web, skills.devops, skills.team],
             bannerPath: YoomyBannerProject,
         },
@@ -30,7 +40,7 @@ export function getWorks() {
             id: 2,
             period: `2022 - ${t("worksSection.table.toNow")}`,
             clientName: "Yoomy",
-            role: createI18ValueWithFromOne("Fullstack developer") satisfies I18nValue,
+            role: createI18ValueWithFromOne("Fullstack developer"),
             skills: [skills.web, skills.vue, skills.app],
             bannerPath: YoomyBannerProject,
         },
@@ -38,7 +48,7 @@ export function getWorks() {
             id: 3,
             period: `2024 - ${t("worksSection.table.toNow")}`,
             clientName: "NGSensors",
-            role: createI18ValueWithFromOne("Web developer") satisfies I18nValue,
+            role: createI18ValueWithFromOne("Web developer"),
             skills: [skills.web, skills.react],
             bannerPath: YoomyBannerProject,
         },
@@ -46,7 +56,7 @@ export function getWorks() {
             id: 4,
             period: `2024 - ${t("worksSection.table.toNow")}`,
             clientName: "Paust Auction",
-            role: createI18ValueWithFromOne("Web developer") satisfies I18nValue,
+            role: createI18ValueWithFromOne("Web developer"),
             skills: [skills.vue, skills.web],
             bannerPath: YoomyBannerProject,
         },
