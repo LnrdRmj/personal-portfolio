a694d0ec39e2cb58cafdcfb98c028344ea912142
LnrdRmj <leonardo.ra14@icloud.com>
2025-01-03 12:24:09 +0100

    doble images uses mediascontainer

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Work } from '../../data/works/works';
 import ProjectHeader from './header/ProjectHeader.vue';
+import DoubleImages from './sections/doubleImages/DoubleImages.vue';
+import PlaceholderImage from '../../assets/placeholderImage.png'
+import PlaceholderVideo from '../../assets/PlaceholderVideo.mp4'
 
 const props = defineProps<{
     project: Work
@@ -9,7 +12,17 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="flex justify-center w-full">
+    <div class="flex flex-col justify-center w-full">
         <ProjectHeader :project="project" />
+        <DoubleImages class="h-[600px]" :medias="[
+            {
+                url: PlaceholderImage,
+                type: 'image'
+            },
+            {
+                url: PlaceholderVideo,
+                type: 'video'
+            }
+        ]" />
     </div>
 </template>
--- src/components/projects/sections/doubleImages/DoubleImages.vue
+++ src/components/projects/sections/doubleImages/DoubleImages.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import MediaContainer from '../common/MediaContainer.vue';
+import { MediaUrl } from '../common/media';
+
+const props = defineProps<{
+    medias: [MediaUrl, MediaUrl]
+}>()
+
+</script>
+
 <template>
-    <div>
-        double images
+    <div class="flex gap-2">
+        <MediaContainer v-for="media in medias" :media="media" class="w-1/2 object-cover h-full" />
     </div>
 </template>
