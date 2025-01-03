162170a100852b9ee0de59ab1ed85a61705e65f3
LnrdRmj <leonardo.ra14@icloud.com>
2025-01-03 12:23:40 +0100

    media container
    accepts both images and videos and displays them

---
+++ src/components/projects/sections/common/MediaContainer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { MediaUrl } from './media';
+
+const props = defineProps<{
+    media: MediaUrl
+}>()
+
+const isImage = props.media.type == 'image'
+const isVideo = !isImage
+
+</script>
+
+<template>
+    <component :is="isImage == true ? 'img' : 'video'" :src="media.url" class="" :autoplay="isVideo" muted loop />
+</template>
---
+++ src/components/projects/sections/common/media.ts
@@ -0,0 +1,4 @@
+export type MediaUrl = {
+    url: string;
+    type: "video" | "image";
+};
