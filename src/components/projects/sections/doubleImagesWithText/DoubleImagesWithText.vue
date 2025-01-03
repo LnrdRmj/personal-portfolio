a68bac3f4746041ab581192f55a89963bfe44bf8
LnrdRmj <leonardo.ra14@icloud.com>
2025-01-03 12:44:54 +0100

    option to switch text position

--- src/components/projects/sections/doubleImagesWithText/DoubleImagesWithText.vue
+++ src/components/projects/sections/doubleImagesWithText/DoubleImagesWithText.vue
@@ -3,16 +3,19 @@ import HeaderCard from '../../header/HeaderCard.vue';
 import { MediaUrl } from '../common/media';
 import DoubleImages from '../doubleImages/DoubleImages.vue';
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
     medias: [MediaUrl, MediaUrl],
     title: string,
     description: string,
-}>()
+    textPosition?: "left" | "right"
+}>(), {
+    textPosition: 'right'
+})
 
 </script>
 
 <template>
-    <div class="flex">
+    <div class="flex" :class="textPosition == 'left' ? 'flex-row-reverse' : 'flex-row'">
         <DoubleImages :medias="medias" class="w-1/2" />
         <div class="w-1/2 px-20 pt-32 flex flex-col">
             <HeaderCard :title="title" :description="description" />
