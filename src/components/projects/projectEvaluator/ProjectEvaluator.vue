4315076beffcfbd6b45e07356da850c8ad4f288b
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-01 10:47:58 +0100

    project and section evaluator

---
+++ src/components/projects/projectEvaluator/ProjectEvaluator.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ProjectSection } from '@/services/project/project';
+import SectionEvaluator from './SectionEvaluator.vue';
+
+defineProps<{
+    sections: ProjectSection[]
+}>()
+
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <div v-for="section of sections">
+            <SectionEvaluator :section="section" class="size-full" />
+        </div>
+    </div>
+</template>
---
+++ src/components/projects/projectEvaluator/SectionEvaluator.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ProjectSection, ProjectSectionToComponent } from '@/services/project/project';
+
+const props = defineProps<{
+    section: ProjectSection
+}>()
+
+</script>
+
+<template>
+    <component :is="ProjectSectionToComponent[section.name]" v-bind="props.section">
+
+    </component>
+</template>
