c07d225d9a7cc623673b5b5a7d2fe1d705dc1e6d
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-08 14:46:03 +0100

    Update ProjectHeader.vue

--- src/components/projects/header/ProjectHeader.vue
+++ src/components/projects/header/ProjectHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Project } from '../../../data/works/project';
+import { Project } from '../../../data/projects/project';
 import { getSelectedLanguageFromI18nValue } from '../../../i18n/i18n';
 import HeaderCard from './HeaderCard.vue';
 
@@ -13,12 +13,15 @@ defineProps<{
         <div class="w-full h-full">
             <img :src="project.bannerPath" alt="" class="size-full object-cover">
         </div>
-        <div class="flex px-20 py-10">
-            <HeaderCard :title="project.clientName" :description="getSelectedLanguageFromI18nValue(project.description)"
-                class="w-1/2" />
-            <div class="w-1/2 flex">
-                <HeaderCard :title="$t('position')" description="Co-Owner / Creative Director" class="w-1/2" />
-                <HeaderCard :title="$t('periodOfTime')" description="2021 - Ora" class="w-1/2" />
+        <div class="flex-center">
+            <div class="flex px-20 py-10 max-w-[1200px]">
+                <HeaderCard :title="project.clientName"
+                    :description="getSelectedLanguageFromI18nValue(project.description)" class="w-1/2" />
+                <div class="w-1/2 flex h-full gap-5">
+                    <HeaderCard :title="$t('position')" :description="getSelectedLanguageFromI18nValue(project.role)"
+                        class="w-1/2" />
+                    <HeaderCard :title="$t('periodOfTime')" :description="project.period" class="w-1/2" />
+                </div>
             </div>
         </div>
     </div>
