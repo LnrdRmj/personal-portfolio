eb35529bd10220145aeed78eae0bd21fdf044514
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-08 14:45:50 +0100

    project page uses porject evaluator

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Project } from '../../data/projects/project';
+import ContactMeSection from '../common/contactMeSection/ContactMeSection.vue';
 import ProjectHeader from './header/ProjectHeader.vue';
 import ProjectEvaluator from './projectEvaluator/ProjectEvaluator.vue';
 
@@ -12,6 +13,9 @@ defineProps<{
 <template>
     <div class="flex flex-col justify-center w-full gap-2">
         <ProjectHeader :project="project" />
-        <ProjectEvaluator :sections="project.sections" />
+        <div class="flex-center w-full">
+            <ProjectEvaluator :sections="project.sections" class="max-w-[1200px]" />
+        </div>
+        <ContactMeSection class="lg:px-64" />
     </div>
 </template>
