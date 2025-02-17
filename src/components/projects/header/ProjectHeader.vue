c08e36ba1dcf9a181a2d860bc246bdd27a5de66d
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-17 11:32:28 +0100

    responsive project header

--- src/components/projects/header/ProjectHeader.vue
+++ src/components/projects/header/ProjectHeader.vue
@@ -22,10 +22,10 @@ function buildProjectPeriod() {
             <img :src="project.bannerPath" alt="" class="size-full object-cover">
         </div>
         <div class="flex-center">
-            <div class="flex px-20 py-10 max-w-[1200px] w-full">
+            <div class="flex flex-col md:flex-row gap-5 md:gap-0 px-20 py-10 max-w-[1200px] w-full">
                 <HeaderCard :title="createI18ValueWithFromOne(project.clientName)" :description="project.description"
-                    class="w-1/2" />
-                <div class="w-1/2 flex h-full gap-5">
+                    class="md:w-1/2" />
+                <div class="md:w-1/2 flex flex-col md:flex-row h-full gap-5">
                     <HeaderCard :title="$t('position')" :description="project.role" class="w-1/2" />
                     <HeaderCard :title="$t('periodOfTime')" :description="buildProjectPeriod()" class="w-1/2" />
                 </div>
