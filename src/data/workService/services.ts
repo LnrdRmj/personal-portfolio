a877d01e8d28335fb57d8d8b4f17956096758a59
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-25 15:56:38 +0100

    updated services

--- src/data/workService/services.ts
+++ src/data/workService/services.ts
@@ -9,31 +9,49 @@ export type WorkService = {
 import ImagesWorkService1 from "./assets/imageService1.png";
 export const workService1: WorkService = {
     description: {
-        it: "Descrizione 1 it",
-        en: "Description 1 en",
+        it: "Rebranding aziendale che comprende la creazione di Palette colori, strategia Social Network e linea di Merchandising.",
+        en: "Corporate rebranding including the creation of color palettes, social network strategy, and a merchandising line.",
     },
     image: ImagesWorkService1,
-    skills: [createI18ValueFromOne("Skill 1")],
+    skills: [
+        createI18ValueFromOne("Flussi Utente (UX)"),
+        createI18ValueFromOne("Website Design (UI)"),
+        createI18ValueFromOne("Figma"),
+        createI18ValueFromOne("Responsive / Mobile"),
+        createI18ValueFromOne("Gestione"),
+    ],
 };
 
 import ImagesWorkService2 from "./assets/imageService2.png";
 export const workService2: WorkService = {
     description: {
-        it: "Descrizione 2 it",
-        en: "Description 2 en",
+        it: "Sviluppo di un’identità visiva conforme ai valori distintivi dell’azienda.",
+        en: "Development of a visual identity in line with the company's distinctive values.",
     },
     image: ImagesWorkService2,
-    skills: [createI18ValueFromOne("Skill 2")],
+    skills: [
+        createI18ValueFromOne("Logo"),
+        createI18ValueFromOne("Palette colori"),
+        createI18ValueFromOne("Figma"),
+        createI18ValueFromOne("Responsive / Mobile"),
+        createI18ValueFromOne("Gestione"),
+    ],
 };
 
 import ImagesWorkService3 from "./assets/imageService3.png";
 export const workService3: WorkService = {
     description: {
-        it: "Descrizione 3 it",
-        en: "Description 3 en",
+        it: "Realizzazione di contenuti multimediali per elevare la presenza sul mercato dell’azienda.",
+        en: "Creation of multimedia content to enhance the company's market presence.",
     },
     image: ImagesWorkService3,
-    skills: [createI18ValueFromOne("Skill 3")],
+    skills: [
+        createI18ValueFromOne("Animazione 3D"),
+        createI18ValueFromOne("Pubblicità"),
+        createI18ValueFromOne("Immagini"),
+        createI18ValueFromOne("Social Network"),
+        createI18ValueFromOne("Merchandising"),
+    ],
 };
 
 export const workServices: WorkService[] = [workService1, workService2, workService3];
