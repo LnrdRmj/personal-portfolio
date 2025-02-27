2e07ecb6b7c7c3e0d365808b6f2d0ee59d76147f
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-27 15:01:49 +0100

    left only one work

--- src/components/landingPage/feturedWorks/FeaturedWorks.vue
+++ src/components/landingPage/feturedWorks/FeaturedWorks.vue
@@ -13,13 +13,12 @@ import { YoomyProject } from "@/data/projects/yoomy/yoomyProject";
         <div
             class="flex flex-col space-y-8 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-7 sm:grid-rows-[repeat(5,minmax(200px,auto))]">
             <WorkCard class="col-span-2 row-span-2" variant="big" :project="YoomyProject" />
-            <WorkCard class="col-span-1 row-start row-span-1" :project="YoomyProject" />
+            <!-- <WorkCard class="col-span-1 row-start row-span-1" :project="YoomyProject" />
             <WorkCard class="col-start-2 col-end-4 row-span-2" variant="big" :project="YoomyProject" />
             <div>
-                <!-- empty -->
             </div>
             <WorkCard class="col-span-1 row-start row-span-1" :project="YoomyProject" />
-            <WorkCard class="col-span-1 row-start row-span-1" :project="YoomyProject" />
+            <WorkCard class="col-span-1 row-start row-span-1" :project="YoomyProject" /> -->
         </div>
     </div>
 </template>
