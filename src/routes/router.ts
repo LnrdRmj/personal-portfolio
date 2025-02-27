6b0f594227aff96768c1fde3bd326267b5ac1f27
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-27 15:07:41 +0100

    return saved position if any

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,10 +4,13 @@ import { routes } from "./routes";
 export const router = createRouter({
     history: createWebHistory(),
     routes,
-    scrollBehavior(to) {
+    scrollBehavior(to, from, savedPosition) {
         if (to.meta.scrollToTop) {
             return { top: 0, behavior: "instant" };
         }
+
+        if (savedPosition) return savedPosition;
+
         return false;
     },
 });
