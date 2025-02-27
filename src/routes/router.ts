7ee3c92bb4318435669465b85b07e718310a61a9
LnrdRmj <leonardo.ra14@icloud.com>
2025-02-27 15:03:09 +0100

    new meta option
    to scroll on top of the page

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,4 +4,10 @@ import { routes } from "./routes";
 export const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to) {
+        if (to.meta.scrollToTop) {
+            return { top: 0, behavior: "instant" };
+        }
+        return false;
+    },
 });
