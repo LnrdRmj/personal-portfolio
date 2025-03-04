6ce762c5478616b459433e0dc0d0aa507cdc785e
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-04 17:26:21 +0100

    Done a bunhc of section for the trackone project

--- src/data/projects/trackone/trackoneProject.ts
+++ src/data/projects/trackone/trackoneProject.ts
@@ -1,12 +1,16 @@
 import { createI18ValueFromOne } from "@/i18n/i18n";
 import { projectSkills } from "../projectSkills";
 
+import {
+    createImageMediaUrl,
+    createVideoMediaUrl,
+} from "@/components/projects/sections/common/media";
+import { Project } from "../project";
+import DevicesPageVideo from "./assets/Devices Video.mp4";
+import DeviceDetailPageVideo from "./assets/Device Detail Video.mp4";
+import MapModesVideo from "./assets/Map Modes.mp4";
 import TrackOneLoginImage from "./assets/trackone-login.png";
 import TrackoneBannerImage from "./assets/trackone_project_banner.png";
-import DevicesPage from "./assets/Devices page.png";
-import DeviceDetailPage from "./assets/Device details.png";
-import { createImageMediaUrl } from "@/components/projects/sections/common/media";
-import { Project } from "../project";
 
 export const TrackOneProject: Project = {
     id: 2,
@@ -44,12 +48,53 @@ export const TrackOneProject: Project = {
             },
             child: {
                 name: "single-image",
-                media: createImageMediaUrl(DevicesPage),
+                media: createVideoMediaUrl(DevicesPageVideo),
             },
         },
         {
-            name: "single-image",
-            media: createImageMediaUrl(DeviceDetailPage),
+            name: "with-title",
+            title: {
+                en: "FEATURES",
+                it: "FEATURES",
+            },
+            description: {
+                en:
+                    "The customer asked for an interactive map where they could analyze the behaviour of the device and the measurements that it gathered throughout time. <br>" +
+                    "Maps are feature rich and you can:" +
+                    "" +
+                    "<li>Have a look at where the device has been</li>" +
+                    "<li>Take a look of each and every single message a device has sent over</li>" +
+                    "<li>Display a chart that displays the values of a measurement throughout time</li>" +
+                    "<li>Change the observed time period</li>" +
+                    "",
+                it: "Maps were built with the help of Leaflet, a powerful library to display and control maps in your site. Have a look!",
+            },
+            child: {
+                name: "single-image",
+                media: createVideoMediaUrl(DeviceDetailPageVideo),
+            },
+        },
+        {
+            name: "with-title",
+            title: {
+                en: "AND EVEN MORE FEATURES",
+                it: "AND EVEN MORE FEATURES",
+            },
+            description: {
+                en:
+                    "There's also \"Map Modes\" that let's thes user change how the points on the map are visualized. Here you can see a bunch of examples but the possibilities are endless.<br>" +
+                    "In the video below you can choose between: " +
+                    "<li>The number of satellites a certain point has</li>" +
+                    "<li>The speed of the device on a certain point colored accordingly</li>" +
+                    "<li>The temperature</li>" +
+                    "<li>The angle the device was facing</li>" +
+                    "",
+                it: "Maps were built with the help of Leaflet, a powerful library to display and control maps in your site. Have a look!",
+            },
+            child: {
+                name: "single-image",
+                media: createVideoMediaUrl(MapModesVideo),
+            },
         },
     ],
 };
