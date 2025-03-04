1c280afe554091205f0f941f95ba44e87076d5a8
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-04 12:08:00 +0100

    added docker skill

--- src/data/projects/projectSkills.ts
+++ src/data/projects/projectSkills.ts
@@ -1,10 +1,11 @@
 export type Skill = { name: string; color: string };
 
-export const projectSkills: Record<string, Skill> = {
+export const projectSkills = {
     web: { name: "WEB", color: "#79F7FF" },
     devops: { name: "DEVOPS", color: "#A7FF7D" },
     team: { name: "TEAM", color: "#79F7FF" },
     vue: { name: "VUE", color: "#A7FF7D" },
     app: { name: "APP", color: "#FF72E0" },
     react: { name: "REACT", color: "#CE90FF" },
-} as const;
+    docker: { name: "DOCKER", color: "#2496ED" },
+} satisfies Record<string, Skill>;
