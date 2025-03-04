e500978615e1401dbf9e2d26c2049270f54d10ca
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-04 10:58:07 +0100

    fix scroll when no animation on route change

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,9 +5,16 @@ import { routerAnimationDuration } from "@/services/animations/animations";
 export const router = createRouter({
     history: createWebHistory(),
     routes,
-    scrollBehavior(to, _, savedPosition) {
+    scrollBehavior(to, from, savedPosition) {
+        const nowRouteAnimation = to.query["noRouteAnimation"] === null;
+        const routeAnimationDuration = nowRouteAnimation ? 0 : routerAnimationDuration; //ms
+
         if (to.meta.scrollToTop) {
-            return { top: 0, behavior: "instant" };
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve({ top: 0, behavior: "instant" });
+                }, routeAnimationDuration);
+            });
         }
 
         if (savedPosition) {
