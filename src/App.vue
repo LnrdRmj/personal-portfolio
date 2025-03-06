3e811efb1951d13679f24cbb6e06f0fb00183a7d
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-06 13:42:13 +0100

    haeder routes

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,34 @@ import { useRoute } from 'vue-router';
 import Header from './components/Header.vue';
 import Footer from './components/landingPage/footer/Footer.vue';
 import { computed } from 'vue';
+import { useTranslation } from 'i18next-vue';
 
 const route = useRoute()
 const noRouteAnimation = computed(() => route.query['noRouteAnimation'] === null)
+
+const { t } = useTranslation()
+
+const headerRoutes = [
+    {
+        title: t('headerSection.services'),
+        onClick: () => { document.querySelector('#service-container')?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' }) },
+    },
+    {
+        title: t('headerSection.work'),
+        onClick: () => { document.querySelector('#works-container')?.scrollIntoView({ behavior: 'smooth', block: 'center' }) },
+    },
+    {
+        title: t('headerSection.whoAmI'),
+        onClick: () => { document.querySelector('#whoami-container')?.scrollIntoView({ behavior: 'smooth', block: 'center' }) },
+    },
+]
 
 </script>
 
 <template>
     <div class="flex flex-col">
         <div class="fixed w-full z-20 h-16">
-            <Header class="w-full h-full"></Header>
+            <Header :routes="headerRoutes" class="w-full h-full"></Header>
         </div>
         <RouterView v-slot="{ Component }">
             <transition :name="noRouteAnimation ? 'no-animation' : 'fade'"
