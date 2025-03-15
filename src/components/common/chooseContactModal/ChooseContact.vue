15e5def8d8cb192185845278bd15b31086ab6a27
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-15 16:45:40 +0100

    choose contact

---
+++ src/components/common/chooseContactModal/ChooseContact.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="max-w-[1000px] w-full h-[750px] flex rounded-3xl overflow-hidden">
+        <div class="w-1/3 h-full bg-orange-400">
+
+        </div>
+        <div class="flex-1 min-w-0 p-12 bg-primary">
+            <div class="size-full bg-slate-600">
+
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/common/chooseContactModal/ChooseContactModal.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Modal from '../modal/Modal.vue';
+import ChooseContact from './ChooseContact.vue';
+
+const open = defineModel('open', { required: true })
+
+</script>
+
+<template>
+    <Modal v-model:open="open">
+        <ChooseContact />
+    </Modal>
+</template>
