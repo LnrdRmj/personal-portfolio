0b31c77d8e6d22c378425d17b81acdf57d93f913
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-15 17:35:13 +0100

    app button

---
+++ src/components/common/buttons/AppButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button
+        class="px-8 w-fit rounded-lg bg-secondary flex-center text-white font-semibold hover:bg-blue-600 transition-colors disabled:bg-black/50">
+        <slot />
+    </button>
+</template>
